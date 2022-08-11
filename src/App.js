608c865a0a4ad5da442dc0923e7dd744dbc23cac
Max Chen <max_chen@trendmicro.com>
2022-08-11 14:10:44 +0800

    setup event listener for alert popup

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      setupEventListener()
     } else {
       console.log("No authorized account found");
     }
@@ -66,10 +67,42 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+      setupEventListener() 
     } catch (error) {
       console.log(error);
     }
   }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   const askContractToMintNft = async () => {
     const CONTRACT_ADDRESS = "0x4c045c4247333a9B877b2aBD29CCa1B243077D93";
@@ -113,7 +146,7 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">Metaverse security NFT Collection</p>
+          <p className="header gradient-text">Metaverse Security NFT Collection</p>
           <p className="sub-text">
             Limited edition. Mint your awesome NFT today.
           </p>
