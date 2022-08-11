e0ca43ae91c210ac24a1dee2ec9ce9165595829c
Max Chen <max_chen@trendmicro.com>
2022-08-11 15:48:06 +0800

    testing button

--- src/App.js
+++ src/App.js
@@ -161,7 +161,7 @@ const App = () => {
           )}
         </div>
         <div>
-          <button onclick={()=> window.open({OPENSEA_LINK}, "_blank")} className="cta-button opensea-button">
+          <button onclick={window.open({OPENSEA_LINK}, "_blank")} className="opensea-button">
             🌊 View Collection on OpenSea
           </button>
         </div>
